87db169e7447239357ae8fb6200fa56692ddaf6f
hunshcn <hunsh.cn@gmail.com>
2024-03-28 22:07:26 +0800

    Update oh-my-zsh.sh

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -3,7 +3,6 @@ set -e
 git clone --quiet --depth 1 https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
 cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
 
-chsh -s $(which zsh)
 sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="af-magic"/g' ~/.zshrc
 
 ZSH_CUSTOM=$HOME"/.oh-my-zsh/custom"
@@ -14,4 +13,5 @@ sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting
 
 echo 'export PS1=${PS1/"%~"/"%m %~"}' >> ~/.zshrc
 
-sudo apt update && sudo apt install jq screen -y
+chsh -s $(which zsh)
+# curl -fL https://github.com/hunshcn/notepad/raw/master/oh-my-zsh.sh | bash
